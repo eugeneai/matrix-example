9ff650fe37575081681767cf7259fb78aac2ce38
Evgeny Cherkashin <eugeneai@irnok.net>
2018-05-21 19:29:27 +0800

    Made the basic specifications.

--- main.cpp
+++ main.cpp
@@ -4,8 +4,42 @@
 
 using namespace std;
 
+typedef vector<vector<float>> matrix_t;
+
+class Matrix {
+
+private:
+  Matrix() = delete;
+
+public:
+  matrix_t m;
+
+  Matrix(int nrows, int ncols);
+  void set(int row, int col, float value);
+  void print(ostream &s);
+};
+
+class Vector {
+
+public:
+  Vector(int size);
+  void set(int index, float value);
+  void print(ostream &s);
+};
+
 int main (int __argc, char **__argv) {
   std::vector<std::string> args(__argv, __argv+__argc);
-  cout << "Hello World!" << endl;
+
+  Matrix B(2,2);
+  B.set(0,0,1);
+  B.set(1,1,2);
+  Vector C(2);
+  C.set(0,1);
+  C.set(1,2);
+
+  cout << "Выходные данные:" << endl;
+  B.print(cout);
+  C.print(cout);
+
   return 0;
 }
