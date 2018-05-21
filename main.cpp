d0546d08b57309d237bf88aaa51af552766bd394
Evgeny Cherkashin <eugeneai@irnok.net>
2018-05-21 19:49:25 +0800

    Implemented all service methods.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 
 using namespace std;
 
-typedef vector<vector<float>> matrix_t;
+typedef vector<float> vector_t;
+typedef vector<vector_t> matrix_t;
 
 class Matrix {
 
@@ -13,6 +14,7 @@ private:
 
 public:
   matrix_t m;
+  int nrows, ncols;
 
   Matrix(int nrows, int ncols);
   void set(int row, int col, float value);
@@ -20,8 +22,12 @@ public:
 };
 
 class Vector {
+  vector_t v;
+
+  Vector() = delete;
 
 public:
+  int size;
   Vector(int size);
   void set(int index, float value);
   void print(ostream &s);
@@ -32,28 +38,42 @@ public:
 
 
 Matrix::Matrix(int nrows, int ncols) {
-
+  this->nrows = nrows;
+  this->ncols = ncols;
+  m = matrix_t(nrows, vector_t(ncols));
 }
-void Matrix::set(int row, int col, float value) {
 
+void Matrix::set(int row, int col, float value) {
+  m[row][col] = value;
 }
 
 void Matrix::print(ostream &s){
-  s << "Выводим Матрицу" << endl;
+  s << "Матрица [" << nrows << ',' << ncols << "]: " << endl;
+  for (int row=0; row<nrows; row++) {
+    s << row << ": ";
+    for (int col=0; col<ncols; col++) {
+      s << m[row][col] << ' ';
+    }
+    s << endl;
+  }
+  s << endl;
 }
 
-
-
-
+// Vector
 
 Vector::Vector(int size) {
-
+  this->size = size;
+  v = vector_t(size);
 }
 void Vector::set(int index, float value) {
-
+  v[index] = value;
 }
 void Vector::print(ostream &s) {
-  s << "Выводим Вектор"  << endl;
+  s << "Вектор [" << size << "]: " ;
+  for (int i=0; i<size; i++) {
+    s << v[i] << ' ';
+  }
+  s << endl;
 }
 
 
@@ -66,9 +86,10 @@ void Vector::print(ostream &s) {
 int main (int __argc, char **__argv) {
   std::vector<std::string> args(__argv, __argv+__argc);
 
-  Matrix B(2,2);
+  Matrix B(2,3); // 2 rows, 3 columns
   B.set(0,0,1);
   B.set(1,1,2);
+  B.set(1,2,3);
   Vector C(2);
   C.set(0,1);
   C.set(1,2);
